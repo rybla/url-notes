0f02b24d1e998b8058c74b75ee53c156a6f048c3
Henry Blanchette <henry@monkeyface.com>
2025-08-12 22:54:05 -0400

    deploy

--- _site/_next/static/D6W8MBb-uCgc1JQEF8EJ-/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fall\u002F[pageIndex]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _site/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VktOm4jkWfbAQsI56Wzm7zLV1Lep2Pp5tSH+73glkCI=\"\n}"
