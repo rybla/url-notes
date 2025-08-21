adc56e59cf9b02fe5f57f0959bd87dc7a200015d
Henry Blanchette <henry@monkeyface.com>
2025-08-20 22:26:24 -0400

    Updates

--- static/chunks/src_app_layout_tsx_68b267f5._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_537b09da._.css"
-],
-    source: "dynamic"
-});
