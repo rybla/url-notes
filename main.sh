df70e4aed79c27cca5bd000afcdd80f226210923
Henry Blanchette <henry@monkeyface.com>
2025-08-22 13:22:06 -0400

    [checkpoint]

--- main.sh
+++ main.sh
@@ -2,16 +2,16 @@
 
 echo "[$(date "+%Y-%m-%d,%H:%M")] [main.sh] begin"
 
-# # pull remote changes
-# git pull -X theirs
+# pull remote changes
+git pull -X theirs
 
-# # update articles
-# bun run script/fetch-articles.ts
+# update articles
+bun run script/fetch-articles.ts
 # bun run script/filter-articles.ts
 # bun run script/process-articles.ts
 
-# # build site
-# bun run build
+# build site
+bun run build
 
 # deploy site
 bun run deploy
