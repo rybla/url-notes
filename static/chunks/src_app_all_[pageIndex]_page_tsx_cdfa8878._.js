7867c79b7f6cfd5402ec4adcc7849835440c738a
Henry Blanchette <henry@monkeyface.com>
2025-08-22 14:02:01 -0400

    Updates

---
+++ static/chunks/src_app_all_[pageIndex]_page_tsx_cdfa8878._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c27dc1e6._.css",
+  "static/chunks/node_modules_next_dist_b758c999._.js"
+],
+    source: "dynamic"
+});
