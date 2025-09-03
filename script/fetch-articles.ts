e8b18d7f1a3dcc11de230e7bcc934c2bc438c182
Henry Blanchette <henry@monkeyface.com>
2025-09-03 12:17:18 -0400

    Fix: Use underscore for filenamifyUrl replacement

--- script/fetch-articles.ts
+++ script/fetch-articles.ts
@@ -39,7 +39,7 @@ async function doArticle(
     config: RssFeedConfig;
   },
 ): Promise<DoArticleResult> {
-  const articleId = filenamifyUrl(url);
+  const articleId = filenamifyUrl(url, { replacement: "_" });
   if (articleIds_ignored.has(articleId)) {
     return "ignored";
   }
